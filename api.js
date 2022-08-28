c3ea51f098ef3d3dcba70d4e7abecd0115bd2a32
mitulchy07 <mitul9616@gmail.com>
2022-08-28 16:39:21 +0200

    add

--- api.js
+++ api.js
@@ -10,8 +10,22 @@
 
 function loadData() {
 
-    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    const url = 'https://jsonplaceholder.typicode.com/users';
     fetch(url)
         .then(response => response.json())
-        .then(json => console.log(json))
+        .then(data => displayUser(data))
 }
+
+function displayUser(data) {
+    const ul = document.getElementById('user-list');
+    for (const user of data) {
+        const li = document.createElement('li');
+        li.innerText = `Users first and last name: ${user.name} ; Username: ${user.username} `;
+        ul.appendChild(li);
+    }
+}
+
+
+
+const stuInfo = JSON.stringify({ name: "James", roll: 3 });
+console.log(stuInfo.name);
